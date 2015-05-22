e248c80f4ab6064dd4a72c93a9b038d814c6b1cb
Maks Nemisj <maks@nemisj.com>
2015-05-22 16:50:18 +0200

    Using rewire module

--- test/ParentComponent.test.js
+++ test/ParentComponent.test.js
@@ -1,11 +1,20 @@
 var React = require('react');
 var expect = require('chai').expect;
+var rewire = require('rewire');
 
-var ParentComponent = require('../ParentComponent.jsx');
+var ParentComponent = rewire('../ParentComponent.jsx');
+
+var ChildMock = React.createClass({
+  render: function () {
+    return <div className="child-mock-component" />;
+  }
+});
+
+ParentComponent.__set__('Child', ChildMock);
 
 describe('ParentComponent', function() {
 
-  var childType = '<div class="child-component"></div>';
+  var childType = React.renderToStaticMarkup(<ChildMock />);
 
   it('should render with child', function() {
     var markup = React.renderToStaticMarkup(<ParentComponent />);
